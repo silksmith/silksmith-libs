2ebceaf71a839ece55e8293d4ddd963b62795ef6
Max Bruchmann <bruchmann@signal7.de>
2014-12-14 18:11:39 +0100

    added angular material + deps

---
+++ angular-aria/src/main/externs/angular-aria.externs.js
@@ -0,0 +1,34 @@
+
+/**
+ * @typedef {{
+ *  $$watchExpr: function(string,string):?
+ * }}
+ */
+angular.$aria;
+
+
+/**
+ * @typedef {{
+ *  ariaHidden: (boolean|undefined),
+ *  ariaChecked: (boolean|undefined),
+ *  ariaDisabled: (boolean|undefined),
+ *  ariaRequired: (boolean|undefined),
+ *  ariaInvalid: (boolean|undefined),
+ *  ariaMultiline: (boolean|undefined),
+ *  ariaValue: (boolean|undefined),
+ *  tabindex: (boolean|undefined),
+ *  bindKeypress: (boolean|undefined)
+ *
+ * }}
+ */
+angular.$AriaProviderConfig;
+/**
+ *
+ */
+angular.$AriaProvider = function(){};
+
+/**
+ *
+ * @param {angular.$AriaProviderConfig} config
+ */
+angular.$AriaProvider.prototype.config = function(config){};
---
+++ hammer.js/src/main/externs/hammer.externs.js
@@ -0,0 +1,17 @@
+/**
+ * @typedef {{
+ * domEvents : (boolean|undefined),
+ * enable : (boolean|undefined),
+ * inputClass : (string|undefined),
+ * inputTargetNull : (string|undefined),
+ * presetArray : (Array.<*>|undefined),
+ * touchAction : (string|undefined)
+ * }}
+ */
+hammer.HammerOptions;
+/**
+ * @param {HTMLElement} element
+ * @param {hammer.HammerOptions=} options
+ * @constructor
+ */
+hammer.Hammer = function(element,options){};
