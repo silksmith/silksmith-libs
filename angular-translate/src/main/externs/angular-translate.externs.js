30ebec39b84ac2cc93d360b1f1515ea7d2907fd1
Max Bruchmann <bruchmann@signal7.de>
2015-01-20 12:54:54 +0100

    more work on translate externs

---
+++ angular-translate/src/main/externs/angular-translate.externs.js
@@ -0,0 +1,112 @@
+/**
+* @suppress {duplicate}
+* @noalias
+*/
+pascalprecht = {};
+/**
+ * @type {Object}
+ * @const
+ */
+pascalprecht.translate = {};
+
+/**
+ * @constructor
+ */
+pascalprecht.translate.$translateProvider = function(){
+};
+/**
+ *
+ * @param {string} key
+ * @param {Object.<string,string>} translationTable
+ */
+pascalprecht.translate.$translateProvider.prototype.translations = function(key, translationTable){};
+/**
+ *
+ * @param {string} lang
+ */
+pascalprecht.translate.$translateProvider.prototype.preferredLanguage = function(lang){};
+/**
+ *
+ * @param {Object} factory
+ */
+pascalprecht.translate.$translateProvider.prototype.addInterpolation = function(factory){};
+/**
+ *
+ * @param {string} name
+ */
+pascalprecht.translate.$translateProvider.prototype.cloakClassName = function(name){};
+/**
+ *
+ * @param {function():string} fn
+ */
+pascalprecht.translate.$translateProvider.prototype.determinePreferredLanguage = function(fn){};
+/**
+ *
+ * @param {(string|Array.<string>)} langKey
+ */
+pascalprecht.translate.$translateProvider.prototype.fallbackLanguage = function(langKey){};
+/**
+ *
+ * @param {string} langKey
+ */
+pascalprecht.translate.$translateProvider.prototype.preferredLanguage = function(langKey){};
+/**
+ *
+ * @param {Array.<string>} languageKeys
+ * @param {Object.<string,string>=} aliases
+ */
+pascalprecht.translate.$translateProvider.prototype.registerAvailableLanguageKeys = function(languageKeys, aliases){};
+/**
+ *
+ * @param {string} key
+ */
+pascalprecht.translate.$translateProvider.prototype.storageKey= function(key){};
+/**
+ *
+ * @param {string} prefix
+ */
+pascalprecht.translate.$translateProvider.prototype.storagePrefix= function(prefix){};
+/**
+ *
+ * @param {string} indicator
+ */
+pascalprecht.translate.$translateProvider.prototype.translationNotFoundIndicator= function(indicator){};
+
+/**
+ *
+ * @para {string} langKey
+ */
+pascalprecht.translate.$translateProvider.prototype.use= function(langKey){};
+
+pascalprecht.translate.$translateProvider.prototype.useCookieStorage= function(){};
+/**
+ *
+ * @param {string} factory
+ */
+pascalprecht.translate.$translateProvider.prototype.useInterpolation= function(factory){};
+/**
+ *
+ * @param {string} loaderFactory
+ * @param {Object=} options
+ */
+pascalprecht.translate.$translateProvider.prototype.useLoader= function(loaderFactory, options){};
+
+//pascalprecht.translate.$translateProvider.prototype.useLoaderCache= function(cache){};
+//
+//pascalprecht.translate.$translateProvider.prototype.useLocalStorage= function(){};
+//
+//pascalprecht.translate.$translateProvider.prototype.useMessageFormatInterpolation= function(){};
+//
+//pascalprecht.translate.$translateProvider.prototype.useMissingTranslationHandler= function(factory){};
+//
+//pascalprecht.translate.$translateProvider.prototype.useMissingTranslationHandlerLog= function(){};
+//
+//pascalprecht.translate.$translateProvider.prototype.usePostCompiling= function(factory){};
+//
+//pascalprecht.translate.$translateProvider.prototype.useSanitizeStrategy= function(value){};
+//
+//pascalprecht.translate.$translateProvider.prototype.useStaticFilesLoader= function(options){};
+//
+//pascalprecht.translate.$translateProvider.prototype.useStorage= function(){};
+//
+//pascalprecht.translate.$translateProvider.prototype.useUrlLoader= function(url, options){};
