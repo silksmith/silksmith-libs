2af5dc0461a098fe2d003988d2c2e3e0d9175025
Max Bruchmann <bruchmann@signal7.de>
2015-03-02 00:44:31 +0800

    bumped lodash

--- lodash/src/main/externs/lodash.externs.js
+++ lodash/src/main/externs/lodash.externs.js
@@ -13,6 +13,26 @@
  */
 function _(obj) {}
 
+_.chunk = function(){}
+_.compact = function(){}
+_.difference = function(){}
+_.drop = function(){}
+_.dropRight = function(){}
+_.dropRightWhile = function(){}
+_.dropWhile = function(){}
+_.fill = function(){}
+_.findIndex = function(){}
+_.findLastIndex = function(){}
+_.first = function(){}
+_.flatten = function(){}
+_.flattenDeep = function(){}
+_.head = function(){}
+_.indexOf = function(){}
+_.initial = function(){}
+_.intersection = function(){}
+_.last = function(){}
+_.lastIndexOf = function(){}
+_.object = function(){}
 /**
  * @template T
  * @param {Array.<T>} obj
@@ -20,15 +40,250 @@ function _(obj) {}
  * @return {Array.<T>}
  */
 _.pull = function(obj, entry) {};
+_.pullAt = function(){}
+_.remove = function(){}
+_.rest = function(){}
+_.slice = function(){}
+_.sortedIndex = function(){}
+_.sortedLastIndex = function(){}
+_.tail = function(){}
+_.take = function(){}
+_.takeRight = function(){}
+_.takeRightWhile = function(){}
+_.takeWhile = function(){}
+_.union = function(){}
+_.uniq = function(){}
+_.unique = function(){}
+_.unzip = function(){}
+_.without = function(){}
+_.xor = function(){}
+_.zip = function(){}
+_.zipObject = function(){}
+_.chain = function(){}
+_.tap = function(){}
+_.thru = function(){}
+_.prototype = function(){}
+_.chain = function(){}
+_.prototype = function(){}
+_.commit = function(){}
+_.prototype = function(){}
+_.plant = function(){}
+_.prototype = function(){}
+_.reverse = function(){}
+_.prototype = function(){}
+_.run = function(){}
+_.prototype = function(){}
+_.toJSON = function(){}
+_.prototype = function(){}
+_.toString = function(){}
+_.prototype = function(){}
+_.value = function(){}
+_.prototype = function(){}
+_.valueOf = function(){}
+_.all = function(){}
+_.any = function(){}
+_.at = function(){}
+_.collect = function(){}
+_.contains = function(){}
+_.countBy = function(){}
+_.detect = function(){}
+_.each = function(){}
+_.eachRight = function(){}
+_.every = function(){}
+_.filter = function(){}
+_.find = function(){}
+_.findLast = function(){}
+_.findWhere = function(){}
+_.foldl = function(){}
+_.foldr = function(){}
+_.forEach = function(){}
+_.forEachRight = function(){}
+_.groupBy = function(){}
+_.include = function(){}
+_.includes = function(){}
+_.indexBy = function(){}
+_.inject = function(){}
+_.invoke = function(){}
+_.map = function(){}
+_.max = function(){}
+_.min = function(){}
+_.partition = function(){}
+_.pluck = function(){}
+_.reduce = function(){}
+_.reduceRight = function(){}
+_.reject = function(){}
+_.sample = function(){}
+_.select = function(){}
+_.shuffle = function(){}
+_.size = function(){}
+_.some = function(){}
+_.sortBy = function(){}
+_.sortByAll = function(){}
+_.where = function(){}
+_.now = function(){}
+_.after = function(){}
+_.ary = function(){}
+_.backflow = function(){}
+_.before = function(){}
+_.bind = function(){}
+_.bindAll = function(){}
+_.bindKey = function(){}
+_.compose = function(){}
+_.curry = function(){}
+_.curryRight = function(){}
+_.debounce = function(){}
+_.defer = function(){}
+_.delay = function(){}
+_.flow = function(){}
+_.flowRight = function(){}
+_.memoize = function(){}
+_.negate = function(){}
+_.once = function(){}
+_.partial = function(){}
+_.partialRight = function(){}
+_.rearg = function(){}
+_.spread = function(){}
+_.throttle = function(){}
+_.wrap = function(){}
+_.clone = function(){}
+_.cloneDeep = function(){}
+_.isArguments = function(){}
+_.isArray = function(){}
+_.isBoolean = function(){}
+_.isDate = function(){}
+_.isElement = function(){}
+_.isEmpty = function(){}
+_.isEqual = function(){}
+_.isError = function(){}
+_.isFinite = function(){}
+_.isFunction = function(){}
+_.isMatch = function(){}
+_.isNaN = function(){}
+_.isNative = function(){}
+_.isNull = function(){}
+_.isNumber = function(){}
+_.isObject = function(){}
+_.isPlainObject = function(){}
+_.isRegExp = function(){}
+_.isString = function(){}
+_.isTypedArray = function(){}
+_.isUndefined = function(){}
+_.toArray = function(){}
+_.toPlainObject = function(){}
+_.inRange = function(){}
+_.random = function(){}
+_.assign = function(){}
+_.create = function(){}
+_.defaults = function(){}
+_.extend = function(){}
+_.findKey = function(){}
+_.findLastKey = function(){}
+_.forIn = function(){}
+_.forInRight = function(){}
+_.forOwn = function(){}
+_.forOwnRight = function(){}
+_.functions = function(){}
+_.has = function(){}
+_.invert = function(){}
 /**
  * @param {Object} obj
  * @return {!Array.<string>}
  */
 _.keys = function(obj) {};
-
+_.keysIn = function(){}
+_.mapValues = function(){}
+_.merge = function(){}
+_.methods = function(){}
+_.omit = function(){}
+_.pairs = function(){}
+_.pick = function(){}
+_.result = function(){}
+_.transform = function(){}
 /**
  * @param {Object.<?, VALUE>} obj
  * @return {!Array.<VALUE>}
  * @template VALUE
  */
 _.values = function(obj) {};
+_.valuesIn = function(){}
+_.camelCase = function(){}
+_.capitalize = function(){}
+_.deburr = function(){}
+_.endsWith = function(){}
+_.escape = function(){}
+_.escapeRegExp = function(){}
+_.kebabCase = function(){}
+_.pad = function(){}
+_.padLeft = function(){}
+_.padRight = function(){}
+_.parseInt = function(){}
+_.repeat = function(){}
+_.snakeCase = function(){}
+_.startCase = function(){}
+_.startsWith = function(){}
+_.template = function(){}
+_.trim = function(){}
+_.trimLeft = function(){}
+_.trimRight = function(){}
+_.trunc = function(){}
+_.unescape = function(){}
+_.words = function(){}
+_.attempt = function(){}
+_.callback = function(){}
+_.constant = function(){}
+_.identity = function(){}
+_.iteratee = function(){}
+_.matches = function(){}
+_.matchesProperty = function(){}
+_.mixin = function(){}
+_.noConflict = function(){}
+_.noop = function(){}
+_.property = function(){}
+_.propertyOf = function(){}
+_.range = function(){}
+_.runInContext = function(){}
+_.times = function(){}
+_.uniqueId = function(){}
+
+_.templateSettings_.imports ={};
+_.templateSettings_.imports._;
+
+/**
+ *
+ * @type {string}
+ */
+_.VERSION;
+_.support = {};
+
+_.support.argsTag;
+
+_.support.enumErrorProps;
+
+_.support.enumPrototypes;
+
+_.support.funcDecomp;
+
+_.support.funcNames;
+
+_.support.nodeTag;
+
+_.support.nonEnumShadows;
+
+_.support.nonEnumStrings;
+
+_.support.ownLast;
+
+_.support.spliceObjects;
+
+_.support.unindexedChars;
+_.templateSettings = {}
+
+_.templateSettings.escape;
+
+_.templateSettings.evaluate;
+
+_.templateSettings.imports;
+
+_.templateSettings.interpolate;
+
+_.templateSettings.variable;
