10af81f58be1a825a0ebf9c94047afb66cd70420
Max Bruchmann <bruchmann@signal7.de>
2015-03-05 00:46:25 +0800

    finished bootstrap externs, not tested

--- bootstrap-js/src/main/externs/bootstrap.externs.js
+++ bootstrap-js/src/main/externs/bootstrap.externs.js
@@ -1,11 +1,17 @@
-
 //todo: can we replace methods string with an enum?
+
+/**
+ *
+ * @namespace
+ */
+bootstrap = {};
 /**
  * @param {string} val
  * @return {!jQuery}
  * @nosideeffects
  */
-jQuery.prototype.off = function(val) {};
+jQuery.prototype.off = function (val) {
+};
 
 /**
  * @param {({
@@ -17,14 +23,16 @@ jQuery.prototype.off = function(val) {};
  * @return {!jQuery}
  * @nosideeffects
  */
-jQuery.prototype.modal = function(opts) {};
+jQuery.prototype.modal = function (opts) {
+};
 
 /**
  * @param {string=} opts
  * @return {!jQuery}
  * @nosideeffects
  */
-jQuery.prototype.dropdown = function(opts) {};
+jQuery.prototype.dropdown = function (opts) {
+};
 
 /**
  * @param {({
@@ -34,7 +42,8 @@ jQuery.prototype.dropdown = function(opts) {};
  * @return {!jQuery}
  * @nosideeffects
  */
-jQuery.prototype.scrollspy = function(opts) {};
+jQuery.prototype.scrollspy = function (opts) {
+};
 
 
 /**
@@ -42,22 +51,108 @@ jQuery.prototype.scrollspy = function(opts) {};
  * @return {!jQuery}
  * @nosideeffects
  */
-jQuery.prototype.tab = function(opts) {};
+jQuery.prototype.tab = function (opts) {
+};
 
+/**
+ *
+ * @constructor
+ */
+bootstrap.TooltipInstance = function () {
+};
 /**
  * @param {({
  *  animation: (boolean|undefined),
  *  container: (string|undefined),
  *  delay: (number|{show: (number|undefined), hide: (number|undefined)}|undefined)
  *  html: (boolean|undefined),
- *  placement: (string|function(HTMLElement):string),
+ *  placement: (string|function(this:bootstrap.TooltipInstance,HTMLElement):string),
  *  selector: (string|undefined),
  *  template: (string|undefined),
  *  title: (string|function():string|undefined),
  *  trigger: (string|undefined),
- *  viewport: (string|{selector: string| padding: (number|undefinde) }|undefined),
+ *  viewport: (string|{selector: string, padding: (number|undefinde) }|undefined),
  * }|string)=} opts
  * @return {!jQuery}
  * @nosideeffects
  */
-jQuery.prototype.tooltip = function(opts) {};
+jQuery.prototype.tooltip = function (opts) {
+};
+
+/**
+ *
+ * @constructor
+ */
+bootstrap.PopoverInstance = function () {
+};
+/**
+ * @param {({
+ *  animation: (boolean|undefined),
+ *  container: (string|undefined),
+ *  delay: (number|{show: (number|undefined), hide: (number|undefined)}|undefined)
+ *  html: (boolean|undefined),
+ *  placement: (string|function(this:bootstrap.PopoverInstance,HTMLElement):string),
+ *  selector: (string|undefined),
+ *  template: (string|undefined),
+ *  title: (string|function():string|undefined),
+ *  trigger: (string|undefined),
+ *  viewport: (string|{selector: string, padding: (number|undefined) }|undefined),
+ * }|string)=} opts
+ * @return {!jQuery}
+ * @nosideeffects
+ */
+jQuery.prototype.popover = function (opts) {
+};
+
+/**
+ * @param {string=} opts
+ * @return {!jQuery}
+ * @nosideeffects
+ */
+jQuery.prototype.alert = function (opts) {
+};
+
+/**
+ * @param {string=} opts
+ * @return {!jQuery}
+ * @nosideeffects
+ */
+jQuery.prototype.button = function (opts) {
+};
+
+
+/**
+ * @param {(
+ *  {parent:(string|undefined), toggle:(boolean|undefined)}|string)=} opts
+ * @return {!jQuery}
+ * @nosideeffects
+ */
+jQuery.prototype.collapse = function (opts) {
+};
+
+/**
+ * @param {(
+ *  {
+ *  interval:(number|undefined),
+ *  pause:(string|undefined),
+ *  wrap:(boolean|undefined),
+ *  keyboard:(boolean|undefined)
+ *  }|number|string)=} opts
+ * @return {!jQuery}
+ * @nosideeffects
+ */
+jQuery.prototype.carousel = function (opts) {
+};
+
+/**
+ * @param {(
+ *  {
+ *  offset:
+ *    (number|function():{bottom: (number|undefined),top: (number|undefined)}|{bottom: (number|undefined),top: (number|undefined)}|undefined),
+ *  target:(string|HTMLElement|jQuery|undefined)
+ *  }|string)=} opts
+ * @return {!jQuery}
+ * @nosideeffects
+ */
+jQuery.prototype.carousel = function (opts) {
+};
